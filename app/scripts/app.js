13536918921e90b23e9bd89885cb46708b32ecea
Max Andre Muller <max.andre.muller+git@gmail.com>
2014-08-24 20:15:39 +0200

    Satt opp med yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name whishlistApp
+ * @description
+ * # whishlistApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('whishlistApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
